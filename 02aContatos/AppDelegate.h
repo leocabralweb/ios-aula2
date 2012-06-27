f6801f63efdf59ad1d4344de2279496297c6544f
ios2749 <ios2749@Mac-mini-de-Caelum-47.local>
2012-06-27 18:36:05 -0300

    Initial Commit

---
+++ 02aContatos/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  02aContatos
+//
+//  Created by ios2749 on 27/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
